22ec5040df4ac28c27c6e25e9e60d47fda11ab52
Alex <turneral1@grinnell.edu>
2017-06-19 12:50:55 -0700

    Commented the abstract interface files and the MCTS agent. Added a run function to the Pacman simulator. Tightened formatting across the agents and the runner file.

--- agents/mcts_agent.py
+++ agents/mcts_agent.py
@@ -4,31 +4,6 @@ from bandits import uniform_bandit_alg
 from heuristics import zero_heuristic
 
 
-def multinomial(p):
-    r = random.random()
-    val = 0
-    for i in range(len(p)):
-        val += p[i]
-        if r <= val:
-            return i
-
-
-class BanditNode:
-    def __init__(self, state, transition_reward, action_list, bandit):
-        self.state = state
-        self.transition_reward = transition_reward
-        self.action_list = action_list
-        self.bandit = bandit
-        self.num_nodes = 0
-
-        """
-        Each action is associated with a dictionary that stores successor bandits/states.
-        The key for each successor is the state. 
-        The value is a list [n,c], where n is a BanditNode and c is the number of times that n has been sampled.
-        """
-        self.children = [{} for i in range(len(action_list))]
-
-
 class MCTSAgentClass(absagent.AbstractAgent):
     myname = "MCTS"
 
@@ -86,10 +61,11 @@ class MCTSAgentClass(absagent.AbstractAgent):
         return root_node.action_list[root_node.bandit.select_best_arm()]
 
     def run_trial(self, node, depth):
-        """
+        """Using agent parameters, recurse up to depth layers from node in the constructed tree.
 
         :param node: points to a BanditNode object. Will not be modified.
-        :param depth:
+        :param depth: how many more layers to generate before using the heuristic.
+        :return total_reward:
         """
 
         if node.bandit is None:  # leaf node
@@ -98,38 +74,73 @@ class MCTSAgentClass(absagent.AbstractAgent):
         current_player = node.state.get_current_player()
         action_index = node.bandit.select_pull_arm()
 
-        # if we reach max children nodes then select randomly among children, otherwise generate a new one
+        # if we reach max children nodes then select randomly among children
         if len(node.children[action_index]) >= self.max_width:
             keys = list(node.children[action_index].keys())
-            counts = [node.children[action_index][k][1] for k in keys]
+            counts = [node.children[action_index][k][1] for k in keys]  # where each key is a state
             normalizer = sum(counts)
-            counts = [c / normalizer for c in counts]
-            successor_index = multinomial(counts)
+            counts = [c / normalizer for c in counts]  # list of counts proportional to total number of samples
+            successor_index = multinomial(counts)  # pseudo-randomly choose a successor
             successor_node = node.children[action_index][keys[successor_index]][0]
             immediate_reward = successor_node.transition_reward
             total_reward = [x + y for (x, y) in zip(immediate_reward, self.run_trial(successor_node, depth - 1))]
-        else:
+        else:  # generate a new successor node
             successor_state = node.state.clone()
+            # reward for taking selected action at this node
             immediate_reward = successor_state.take_action(node.action_list[action_index])
             successor_actions = successor_state.get_actions()
 
+            # if the successor state is already a child
             if successor_state in node.children[action_index]:
                 successor_node = node.children[action_index][successor_state][0]
+                # increment how many times successor_state has been sampled
                 node.children[action_index][successor_state][1] += 1
+                # recurse downwards into the constructed tree
                 total_reward = [x + y for (x, y) in zip(immediate_reward, self.run_trial(successor_node, depth - 1))]
             else:
-                if successor_state.is_terminal() or depth == 1:
+                if successor_state.is_terminal() or depth == 1:  # indicate it's time to use the heuristic
                     successor_bandit = None
-                elif self.bandit_parameters is None:
+                elif self.bandit_parameters is None:  # create a bandit according to how many actions are available
                     successor_bandit = self.BanditAlgClass(len(successor_actions))
                 else:
                     successor_bandit = self.BanditAlgClass(len(successor_actions), self.bandit_parameters)
 
                 successor_node = BanditNode(successor_state, immediate_reward, successor_actions, successor_bandit)
                 node.children[action_index][successor_state] = [successor_node, 1]
-                self.num_nodes += 1
+                self.num_nodes += 1  # we've made a new BanditNode
 
                 total_reward = [x + y for (x, y) in zip(immediate_reward, self.heuristic.evaluate(successor_state))]
 
         node.bandit.update(action_index, total_reward[current_player - 1])
         return total_reward
+
+
+class BanditNode:
+    """Stores information on a state, reward for reaching the state, actions available, and the bandit to be used."""
+    def __init__(self, state, transition_reward, action_list, bandit):
+        self.state = state
+        self.transition_reward = transition_reward
+        self.action_list = action_list
+        self.bandit = bandit
+        self.num_nodes = 0
+
+        """
+        Each action is associated with a dictionary that stores successor bandits/states.
+        The key for each successor is the state. 
+        The value is a list [n,c], where n is a BanditNode and c is the number of times that n has been sampled.
+        """
+        self.children = [{} for i in range(len(action_list))]
+
+
+def multinomial(p):
+    """
+
+    :param p:
+    :return:
+    """
+    r = random.random()
+    val = 0
+    for i in range(len(p)):
+        val += p[i]
+        if r <= val:
+            return i
