08fbff49012aa5768c8e63b6f0abbc3b611c2159
Alex <turneral1@grinnell.edu>
2017-07-05 18:59:11 -0700

    Caught exceptions raised when multiple stats_recorder objects are closing and trying to write - it doesn't matter if they can't write, since we gather the info and write at the end of the process anyways. Fixed an issue with openai_sim's clone function where env would be copied incorrectly. openai_sim now displays links in the table if the user enables uploading. Further generalized openai_sim's __hash__ function. Better modularized openai_sim.

--- demos/runner.py
+++ demos/runner.py
@@ -28,8 +28,8 @@ if __name__ == '__main__':  # for multiprocessing compatibility
 
     all_agents = [u_ro, nested_u_ro, e_ro, ucb_ro, ss_d2, ss_d5, uct, e_root_uct, switch_agent, e_switch_agent]
 
-    openai = openai_sim.OpenAIStateClass(sim_name='FrozenLake-v0', api_key='sk_brIgt2t3TLGjd0IFrWW9rw')
-    openai.run(agents=[uct], num_trials=10, multiprocess=True, show_moves=False, upload=True)
+    openai = openai_sim.Dealer(api_key='sk_brIgt2t3TLGjd0IFrWW9rw')
+    openai.run(sim_name='FrozenLake-v0', agents=[uct], num_trials=10, multiprocess=True, show_moves=False, upload=True)
 
     pacman = pacman_sim.PacmanStateClass(layout_repr='testClassic', use_graphics=True)
     #pacman.run(agents=[uct], num_trials=10)
--- simulators/openai_sim.py
+++ simulators/openai_sim.py
@@ -11,86 +11,28 @@ from gym import wrappers
 from abstract import absstate, absagent
 
 
-class OpenAIStateClass(absstate.AbstractState):
-    """An interface to run bandit algorithms on the OpenAI environment library.
-
-    The simulator can be found at https://github.com/openai/gym.
-
-    Note that unlike other interfaces, this is not compatible with the dealer simulator due its reliance on the openAI
-    engine.
-    """
-
-    def __init__(self, sim_name, api_key=None, force=True):
-        """Initialize an interface with the specified OpenAI simulation task and policy.
-
-        :param sim_name: a valid env key that corresponds to a particular game / task.
-        :param api_key: API key for uploading results to OpenAI Gym. Note that submissions are only scored if they are
-            run for at least 100 trials.
-        :param force: whether an existing directory at demos/OpenAI results/wrapper_target/ should be overwritten
-        """
-        self.api_key = api_key
+class Dealer:
+    def __init__(self, sim_name=None, force=True, api_key=None):
         self.force = force
+        self.api_key = api_key
         self.resume = False  # whether to add data to the output directory
-        self.show_moves = True
-        self.agent = None
-
-        self.load_env(sim_name)
-
-    def reinitialize(self):
-        """Reinitialize the environment."""
-        self.current_observation = self.env.reset()
-        self.done = False
-
-    def clone(self):
-        new_sim = copy.copy(self)
-        new_sim.env = new_sim.env.unwrapped
-        return new_sim
-
-    def set(self, sim):
-        self.env = copy.copy(sim.env.unwrapped)
-        self.current_observation = sim.current_observation
-        self.done = sim.done
-
-    def set_agent(self, agent):
-        self.agent = Agent(agent, self)
-
-    def load_env(self, sim_name):
-        self.sim_name = sim_name
-        self.env = gym.make(sim_name)
-        self.my_name = self.env.spec._env_name
-
-        # output directory location for agent performance
-        self.wrapper_target = 'OpenAI results\\' + self.sim_name[:-3]  # cut off version name
-        self.env = wrappers.Monitor(self.env, self.wrapper_target, write_upon_reset=True, force=self.force,
-                                    resume=self.resume)
-
-        self.action_space = self.env.action_space
-        if not isinstance(self.action_space, spaces.Discrete) and not isinstance(self.action_space, spaces.Tuple):
-            raise ValueError('Action space {} incompatible with {} (only supports Discrete and Tuple action spaces).'
-                             .format(self.action_space, self))
-        self.observation_space = self.env.observation_space
-
-        self.original_observation = self.env.reset()  # initial observation
-        self.current_observation = self.original_observation
-        self.done = False  # indicates if the current observation is terminal
+        self.simulator = OpenAIStateClass(dealer=self, sim_name=sim_name)
 
     def run_all(self, agents, num_trials, multiprocess=False, show_moves=False):
         """Runs the agents on all available simulators."""
         all_environments = gym.envs.registry.all()
         for env_idx, env in enumerate(all_environments):
-            if should_skip(env.id):  # duplicate games / games that hang
-                print("Unwanted game - skipping {}.".format(env.id))
+            if self.should_skip(env.id):  # duplicate games / games that hang
+                print("Filtered game - skipping {}.".format(env.id))
                 continue
-            try:
-                self.load_env(env.id)
-            except ValueError:  # If the action space is continuous
-                print("Continuous action space - skipping {}.".format(env.id))
-                continue
-            self.run(agents=agents, num_trials=num_trials, multiprocess=multiprocess, show_moves=show_moves)
 
-    def run(self, agents, num_trials, multiprocess=True, show_moves=True, upload=False):
+            self.run(sim_name=env.id, agents=agents, num_trials=num_trials, multiprocess=multiprocess,
+                     show_moves=show_moves)
+
+    def run(self, sim_name, agents, num_trials, multiprocess=True, show_moves=True, upload=False):
         """Run the given number of trials on the specified agents, comparing their performance.
 
+        :param sim_name: the simulation to run.
         :param agents: the agents with which to run trials. Should be instances of AbstractAgent.
         :param num_trials: how many trials to be run.
         :param multiprocess: whether to use multiprocessing. Some games (such as Space Invaders) are not compatible
@@ -98,8 +40,14 @@ class OpenAIStateClass(absstate.AbstractState):
         :param show_moves: if the environment can render, then render each move.
         :param upload: whether to upload results to OpenAI.
         """
+        try:
+            self.simulator.load_env(sim_name)
+        except ValueError:  # If the action space is continuous
+            print("Continuous action space - skipping {}.".format(sim_name))
+            return
+
         for agent in agents:
-            agent = Agent(agent, self)
+            agent = Agent(agent, self.simulator)
             if not isinstance(agent.policy, absagent.AbstractAgent):  # if this isn't a planning agent
                 multiprocess = False
                 logging.warning('Multiprocessing is disabled for agents which learn over multiple episodes.')
@@ -128,17 +76,18 @@ class OpenAIStateClass(absstate.AbstractState):
                 row.append(output['url'])
             table.append(row)
         print("\n" + tabulate.tabulate(table, headers, tablefmt="grid", floatfmt=".4f"))
-        print("Each agent ran {} game{} of {}.".format(num_trials, "s" if num_trials > 1 else "", self.my_name))
+        print("Each agent ran {} game{} of {}.".format(num_trials, "s" if num_trials > 1 else "",
+                                                       self.simulator.my_name))
 
     def run_trials(self, agent, num_trials=1, multiprocess=True, upload=False):
         """Run the given number of trials using the current configuration."""
-        self.set_agent(agent)
+        self.simulator.set_agent(agent)
 
         # We need to reinitialize the Monitor for the new trials we are about to run
-        if not self.env.enabled:
-            self.env = gym.make(self.sim_name)
-            self.env = wrappers.Monitor(self.env, self.wrapper_target, write_upon_reset=True, force=self.force,
-                                        resume=self.resume)
+        if not self.simulator.env.enabled:
+            self.simulator.env = gym.make(self.simulator.sim_name)
+            self.simulator.env = wrappers.Monitor(self.simulator.env, self.simulator.wrapper_target,
+                                                  write_upon_reset=True, force=self.force, resume=self.resume)
 
         game_outputs = []
         if multiprocess:
@@ -150,14 +99,14 @@ class OpenAIStateClass(absstate.AbstractState):
                 game_outputs.append(self.run_trial(i))
 
         total_reward, wins, total_time, total_steps = 0, 0, 0, 0
-        stats_recorder = self.env.stats_recorder
+        stats_recorder = self.simulator.env.stats_recorder
         for output in game_outputs:
             total_reward += output['reward']
             wins += output['won']
             total_time += output['total time']
             total_steps += output['episode length']
 
-            # record episode information
+            # Record episode information
             if multiprocess:
                 stats_recorder.timestamps.append(output['timestamp'])
                 stats_recorder.episode_lengths.append(output['episode length'])
@@ -166,10 +115,10 @@ class OpenAIStateClass(absstate.AbstractState):
 
         stats_recorder.flush()
 
-        self.env.close()
+        self.simulator.env.close()
         url = None
         if self.api_key and upload:
-            url = gym.upload(self.wrapper_target, api_key=self.api_key)
+            url = gym.upload(self.simulator.wrapper_target, api_key=self.api_key)
 
         return {'name': agent.agent_name, 'average reward': total_reward / num_trials,
                 'success rate': wins / num_trials,
@@ -190,25 +139,96 @@ class OpenAIStateClass(absstate.AbstractState):
 
         :param trial_num: a placeholder parameter for compatibility with multiprocessing.Pool.
         """
-        self.reinitialize()
-        self.env.video_recorder.enabled = self.show_moves
+        self.simulator.reinitialize()  # #TODO: Initialize simulator for each thread? https://stackoverflow.com/questions/41071563/python-multiprocessing-cant-pickle-thread-lock-pymongo
+        #self.simulator.env.video_recorder.enabled = self.show_moves
         total_time = 0
-        while self.done is False:
+        while not self.simulator.is_terminal():
             begin = time.time()
-            action = self.agent.act()
+            action = self.simulator.agent.act()
             total_time += time.time() - begin
 
-            self.current_observation, reward, self.done, _ = self.env.step(action)
+            rewards = self.simulator.take_action(action)
 
-            if self.show_moves and 'human' in self.env.metadata['render.modes']:  # don't render if not supported
-                self.env.render()  # TODO: investigate CartPole-v0
+            # Don't render if not supported
+            if self.show_moves and 'human' in self.simulator.env.metadata['render.modes']:
+                self.simulator.env.render()  # TODO: investigate CartPole-v0
 
-        return {'reward': self.env.stats_recorder.rewards,
-                'won': reward > 0,  # won if reward after game ends is positive
+        stats_recorder = self.simulator.env.stats_recorder
+        return {'reward': stats_recorder.rewards,
+                'won': rewards[0] > 0,  # won if reward after game ends is positive
                 'total time': total_time,
-                'episode length': self.env.stats_recorder.total_steps,
-                'timestamp': self.env.stats_recorder.timestamps[0],
-                'episode type': self.env.stats_recorder.type}
+                'episode length': stats_recorder.total_steps,
+                'timestamp': stats_recorder.timestamps[0],
+                'episode type': stats_recorder.type}
+
+    @staticmethod
+    def should_skip(game_id):
+        """Returns True if the game should be skipped."""
+        skip_games = ["Assault", "BankHeist", "BeamRider-v4", "CliffWalking"]  # games take a very long time
+        skip_keywords = ["Deterministic", "Frameskip", "ram"]
+        for key in skip_games + skip_keywords:
+            if str.find(game_id, key) != -1:
+                return True
+
+
+class OpenAIStateClass(absstate.AbstractState):
+    """An interface to run bandit algorithms on the OpenAI environment library.
+
+    The simulator can be found at https://github.com/openai/gym.
+
+    Note that unlike other interfaces, this is not compatible with the dealer simulator due its reliance on the openAI
+    engine.
+    """
+
+    def __init__(self, dealer, sim_name=None):
+        """Initialize an interface with the specified OpenAI simulation task and policy.
+
+        :param sim_name: a valid env key that corresponds to a particular game / task.
+        :param api_key: API key for uploading results to OpenAI Gym. Note that submissions are only scored if they are
+            run for at least 100 trials.
+        :param force: whether an existing directory at demos/OpenAI results/wrapper_target/ should be overwritten
+        """
+        self.agent = None
+        self.dealer = dealer
+        if sim_name:
+            self.load_env(sim_name)
+
+    def reinitialize(self):
+        """Reinitialize the environment."""
+        self.current_observation = self.env.reset()
+        self.done = False
+
+    def clone(self):
+        new_sim = copy.copy(self)
+        new_sim.env = new_sim.env.unwrapped
+        return new_sim
+
+    def set(self, sim):
+        self.env = copy.copy(sim.env.unwrapped)
+        self.current_observation = sim.current_observation
+        self.done = sim.done
+
+    def set_agent(self, agent):
+        self.agent = Agent(agent, self)
+
+    def load_env(self, sim_name):
+        self.sim_name = sim_name
+        self.env = gym.make(sim_name)
+        self.my_name = self.env.spec._env_name
+
+        # output directory location for agent performance
+        self.wrapper_target = 'OpenAI results\\' + self.sim_name[:-3]  # cut off version name
+        self.env = wrappers.Monitor(self.env, self.wrapper_target, write_upon_reset=True, force=self.dealer.force,
+                                    resume=self.dealer.resume)
+
+        self.action_space = self.env.action_space
+        if not isinstance(self.action_space, spaces.Discrete) and not isinstance(self.action_space, spaces.Tuple):
+            raise ValueError('Action space {} incompatible with {} (only supports Discrete and Tuple action spaces).'
+                             .format(self.action_space, self))
+        self.observation_space = self.env.observation_space
+
+        self.current_observation = self.env.reset()  # initial observation
+        self.done = False  # indicates if the current observation is terminal
 
     def number_of_players(self):
         return 1
@@ -248,7 +268,6 @@ class OpenAIStateClass(absstate.AbstractState):
             return hash(self.current_observation)
 
 
-
 class Agent(object):
     """An agent that interfaces between a policy and the OpenAI environment."""
     def __init__(self, policy, parent):
@@ -257,13 +276,4 @@ class Agent(object):
 
     def act(self):
         """Despite what its name may suggest, act only determines what action to take."""
-        return self.policy.select_action(self.parent)  # TODO: fix for UCT
-
-
-def should_skip(game_id):
-    """Returns True if the game should be skipped."""
-    skip_games = ["Assault", "BankHeist", "BeamRider-v4", "CliffWalking"]  # games take a very long time
-    skip_keywords = ["Deterministic", "Frameskip", "ram"]
-    for key in skip_games + skip_keywords:
-        if str.find(game_id, key) != -1:
-            return True
+        return self.policy.select_action(self.parent)
