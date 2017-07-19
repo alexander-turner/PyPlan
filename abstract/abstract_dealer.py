3d296857f3aab38d89c1a571c46193a875a8023b
Alex <turneral1@grinnell.edu>
2017-07-19 12:09:03 -0700

    Reorganized simulators to distinguish between dealers and the simulator interfaces they use. Added abstract_dealer file. Cleaned up abstract file names. Added available_configurations function to dealers, returning available layouts / simulators.

---
+++ abstract/abstract_dealer.py
@@ -0,0 +1,40 @@
+import abc
+from abc import ABCMeta
+
+
+class AbstractDealer:
+    """Dealers facilitate simulation and display results in an easy-to-read format.
+
+    Simulators which are incompatible with native_dealer.py should have their own dealer. Compatibility with
+     multiprocessing and tabulate is encouraged. openai_sim.py and pacman_sim.py contain implementation examples. If
+     the dealer has access to multiple environments or game configurations / layouts, the available_configurations
+     function should be provided.
+    """
+    __metaclass__ = ABCMeta
+
+    @abc.abstractmethod
+    def run(self, agents, num_trials=1, multiprocess=True, show_moves=False):
+        """Handles data processing, initiates run_trials for each of the provided agents, and displays results.
+
+        :param agents: the agents whose performance will be compared.
+        :param num_trials: how many times the simulation will be run.
+        :param multiprocess: whether to speed the computation with parallel processing.
+        :param show_moves: whether moves should be rendered. Disabled if multiprocess is True.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def run_trials(self, agent):
+        """Run a given number of trials using the current configuration, recording and returning performance statistics.
+
+        :param agent: the agent to be used in the trials.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def run_trial(self, trial_num):
+        """Runs a single iteration of the simulator with the given agent.
+
+        :param trial_num: a placeholder parameter for compatibility with multiprocessing.Pool.
+        """
+        raise NotImplementedError
