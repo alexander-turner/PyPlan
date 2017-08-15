6d2efcc09909127ad5f23cff0f882a5446a42420
Alex <turneral1@grinnell.edu>
2017-08-15 14:58:38 -0700

    Started new demo to compare simple and cumulative regret for various bandit algorithms.

---
+++ demos/generate_regret_curves.py
@@ -0,0 +1,40 @@
+import copy
+import matplotlib.pyplot as plt
+
+
+def generate_regret_curves(agents, pull_values, simulator, num_trials=50):
+    """Generate and graph regret curves for agents x pull_values on simulator.
+
+    :param agents: one of each agent for which regret curves will be generated.
+    :param pull_values: a list of pull values (with which agents will be initialized).
+    :param simulator: assume simulator outputs cumulative and simple regrets.
+    :param num_trials: for how many trials we will run
+    """
+    # Generate permutations of agents using pull values
+    new_agents = []
+    for agent in agents:
+        for num_pulls in pull_values:
+            new_agent = copy.deepcopy(agent)
+            new_agent.num_pulls = num_pulls
+            new_agents.append(new_agent)
+
+    # For each agent, initialize average regret
+    cumulative, simple = {}, {}
+    for agent in new_agents:
+        cumulative[agent], simple[agent] = [0], [0]
+
+    for agent in new_agents:
+        for trial in range(1, num_trials + 1):
+            c, s = trial, trial*2
+            #c, s = simulator(agent)
+            cumulative[agent].append((cumulative[agent][trial-1] * (trial - 1) + c) / trial)
+            simple[agent].append((simple[agent][trial-1] * (trial - 1) + s) / trial)
+
+    plt.figure(1)
+    plt.xlabel('Trial #')
+    for subplot in (211, 212):
+        plt.subplot(subplot)
+        dict = cumulative if subplot == 212 else simple
+        for agent in new_agents:
+            plt.plot(dict[agent][1:], range(1, num_trials + 1), label=agent.name)
+    plt.show()
---
+++ demos/pacman_comparison.py
@@ -0,0 +1,37 @@
+from agents import *
+from dealers import pacman_dealer
+from demos import generate_regret_curves
+
+"""
+
+"""
+
+if __name__ == '__main__':
+    u_ro = uniform_rollout_agent.UniformRolloutAgent(depth=1, num_pulls=10)
+    e_ro = e_rollout_agent.ERolloutAgent(depth=1, num_pulls=10)
+    ucb_ro = ucb_rollout_agent.UCBRolloutAgent(depth=1, num_pulls=10, c=1.0)
+    agent_list = [u_ro, e_ro, ucb_ro]
+    pull_values = [10, 50, 100]
+
+    pacman = pacman_dealer.Dealer(layout_representation='testClassic')
+    generate_regret_curves.generate_regret_curves(agent_list, pull_values, pacman)
+    """
+    u_ro_10 = uniform_rollout_agent.UniformRolloutAgent(depth=1, num_pulls=10)
+    u_ro_50 = uniform_rollout_agent.UniformRolloutAgent(depth=1, num_pulls=50)
+    u_ro_100 = uniform_rollout_agent.UniformRolloutAgent(depth=1, num_pulls=100)
+
+    e_ro_10 = e_rollout_agent.ERolloutAgent(depth=1, num_pulls=10)
+    e_ro_50 = e_rollout_agent.ERolloutAgent(depth=1, num_pulls=50)
+    e_ro_100 = e_rollout_agent.ERolloutAgent(depth=1, num_pulls=100)
+
+    ucb_ro_10 = ucb_rollout_agent.UCBRolloutAgent(depth=1, num_pulls=10, c=1.0)
+    ucb_ro_50 = ucb_rollout_agent.UCBRolloutAgent(depth=1, num_pulls=50, c=1.0)
+    ucb_ro_100 = ucb_rollout_agent.UCBRolloutAgent(depth=1, num_pulls=100, c=1.0)
+
+    agent_list = [u_ro_10, u_ro_50, u_ro_100,
+                  e_ro_10, e_ro_50, e_ro_100,
+                  ucb_ro_10, ucb_ro_50, ucb_ro_100]
+
+    pacman = pacman_dealer.Dealer(layout_representation='testClassic')
+    pacman.run(agents=agent_list, num_trials=20)
+    """
